d15d2d5776ca1c0eef4804618e2275b70e307634
Joe Rupertus <joerup2004@gmail.com>
2025-09-27 17:33:39 -0400

    grid

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import { initCanvasBackground } from "./canvasBackground.js";
 import { initResumePanel } from "./ui/resumePanel.js";
+import { initGameGrid } from "./ui/gameGrid.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initCanvasBackground();
   initResumePanel();
+
+  const gameGrid = initGameGrid(10);
+  const gridContainer = gameGrid.render();
+
+  const uiOverlay = document.querySelector('.ui-overlay');
+  if (uiOverlay) {
+    uiOverlay.appendChild(gridContainer);
+  } else {
+    document.body.appendChild(gridContainer);
+  }
 });
